69ba1951c7be4a68ec13551c426d5b91e97b0b20
PNYWT <punyawat.suwannatat@hotmail.com>
2023-03-10 00:06:37 +0700

    update
    useEffect + API

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,196 +1,33 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import { useState } from "react";
-
-const resetTextField = {
-  nameContent: "",
-  nameAuthor: "",
-};
+// import KeyBoardManager from "./Component/KeyBoardManager"; <KeyBoardManager>ใช้ก็เอาไปใส่ใน Return </KeyBoardManager>
 
 function App() {
-  //Func input didchange note ใส่ข้อความใหม่แทนอันเก่า
-  const [note, setNote] = useState({
-    nameContent: "",
-    nameAuthor: "",
-  });
-  function noteValueChange(event) {
-    const { name, value } = event.target;
-    setNote((oldNote) => {
-      return {
-        ...oldNote,
-        [name]: value,
-      };
-    });
-  }
+  const [users, setUsers] = useState([]);
 
-  //Func event submit Note
-  const [allNotes, setAllNotes] = useState([]);
-  function allNotesSubmit(event) {
-    event.preventDefault();
-    setAllNotes((oldAllNotes) => {
-      const newNote = { ...note };
-      newNote.id = Date.now().toString(); //เพิ่มอะไรเข้าไปก็ได้เหมือนป้ายชื่อให้ note แต่ละชิ้น คล้ายๆ กับการเพิ่ม model เข้าไปเอง
+  useEffect(() => {
+    fetch("https://reqres.in/api/users?page=2").then(res =>{
+      return res.json();
+    }).then(resJson =>{
+      setUsers(resJson.data);
+    })
+  }, []); 
 
-      let today = new Date(); //ลองเพิ่มการแสดงวันที่เข้าไป
-      let dateTimeSubmit =
-        "Date " +
-        today.getFullYear() +
-        "-" +
-        (today.getMonth() + 1) +
-        "-" +
-        today.getDate() +
-        " Time: " +
-        today.getHours() +
-        ":" +
-        today.getMinutes() +
-        ":" +
-        today.getSeconds();
-      newNote.dateTime = dateTimeSubmit;
-      return [newNote, ...oldAllNotes]; //อันใหม่พอกด submit จะแทรกด้านบน
-      // [...oldAllNotes, note]; อันใหม่พอกด submit จะต่อท้ายไปเรื่อยๆ
-    });
-    setNote(resetTextField);
-  }
-
-  //func delete
-  function deleteNote(note_ID) {
-    setAllNotes((oldNote) => {
-      return oldNote.filter((oneNote) => {
-        return oneNote.id !== note_ID;
-      });
-    });
-  }
-
-  //func Edit
-  const [editNote, setEditNote] = useState(null);
-  let editNoteElement = null;
-  if (!!editNote) {
-    // editNote != nil
-    editNoteElement = (
-      <div className="app-edit-note">
-        <form onSubmit={EditNoteSubmit}>
-          <p>
-            <textarea
-              rows="3"
-              placeholder="Name Content"
-              name="nameContent"
-              value={editNote.nameContent}
-              onChange={editNoteValueChange}
-            ></textarea>
-          </p>
-          <p>
-            <input
-              type="text"
-              placeholder="Name Author"
-              name="nameAuthor"
-              value={editNote.nameAuthor}
-              onChange={editNoteValueChange}
-            ></input>
-          </p>
-          <p>
-            <button type="submit">Click to Add</button>
-          </p>
-        </form>
-      </div>
-    );
-  }
-  function editNoteValueChange(event) {
-    const { name, value } = event.target;
-    setEditNote((noteBeforeEdit) => {
-      return {
-        ...noteBeforeEdit,
-        [name]: value,
-      };
-    });
-  }
-  function EditNoteSubmit(event) {
-    event.preventDefault();
-    setAllNotes((noteBeforeEdit) => {
-      return noteBeforeEdit.map((oneEditNote) => {
-        if (oneEditNote.id !== editNote.id) {
-          return oneEditNote;
-        } else {
-          const noteAfterEdit = { ...editNote };
-          let today = new Date(); //ลองเพิ่มการแสดงวันที่เข้าไป
-          let dateTimeSubmit =
-            "Date " +
-            today.getFullYear() +
-            "-" +
-            (today.getMonth() + 1) +
-            "-" +
-            today.getDate() +
-            " Time: " +
-            today.getHours() +
-            ":" +
-            today.getMinutes() +
-            ":" +
-            today.getSeconds();
-          editNote.dateTime = dateTimeSubmit + "(Edit)";
-          return editNote;
-        }
-      });
-    });
-    setEditNote(null);
-  }
-
-  //Elements UI ListNote
-  const noteElements = allNotes.map((oneNote) => {
-    return (
-      <div key={oneNote.id} className="app-note">
-        <h4>Author: {oneNote.nameAuthor}</h4>
-        <p>Content: {oneNote.nameContent}</p>
-        <p>{oneNote.dateTime}</p>
-        <p>
-          <a
-            onClick={() => {
-              setEditNote(oneNote);
-            }}
-          >
-            Edit
-          </a>
-          <span> | </span>
-          <a
-            onClick={() => {
-              deleteNote(oneNote.id);
-            }}
-          >
-            Delete
-          </a>
-        </p>
-      </div>
-    );
-  });
+  const usersModel = users.map(user =>{
+    return <div>
+      <h3>{user.first_name} {user.last_name}</h3>
+      <p>email : {user.email}</p>
+      <br></br>
+    </div>
+  })
 
   //return UI
   return (
     <section className="app-section">
       <div className="app-container">
-        <h3>Hello React</h3>
-        <form onSubmit={allNotesSubmit}>
-          <p>
-            <textarea
-              rows="3"
-              placeholder="Name Content"
-              name="nameContent"
-              value={note.nameContent}
-              onChange={noteValueChange}
-            ></textarea>
-          </p>
-          <p>
-            <input
-              type="text"
-              placeholder="Name Author"
-              name="nameAuthor"
-              value={note.nameAuthor}
-              onChange={noteValueChange}
-            ></input>
-          </p>
-          <p>
-            <button type="submit">Click to Add</button>
-          </p>
-        </form>
-        <div className="app-notes">{noteElements}</div>
+        <h1>List API</h1>
+        {usersModel}
       </div>
-      {editNoteElement}
     </section>
   );
 }
---
+++ hello-react/src/Component/KeyBoardManager.js
@@ -0,0 +1,195 @@
+import { useState } from "react";
+import "./KeyBoardManager.css";
+const resetTextField = {
+  nameContent: "",
+  nameAuthor: "",
+};
+
+function KeyBoardManager() {
+  const [note, setNote] = useState({
+    nameContent: "",
+    nameAuthor: "",
+  });
+  function noteValueChange(event) {
+    const { name, value } = event.target;
+    setNote((oldNote) => {
+      return {
+        ...oldNote,
+        [name]: value,
+      };
+    });
+  }
+
+  //Func event submit Note
+  const [allNotes, setAllNotes] = useState([]);
+  function allNotesSubmit(event) {
+    event.preventDefault();
+    setAllNotes((oldAllNotes) => {
+      const newNote = { ...note };
+      newNote.id = Date.now().toString(); //เพิ่มอะไรเข้าไปก็ได้เหมือนป้ายชื่อให้ note แต่ละชิ้น คล้ายๆ กับการเพิ่ม model เข้าไปเอง
+
+      let today = new Date(); //ลองเพิ่มการแสดงวันที่เข้าไป
+      let dateTimeSubmit =
+        "Date " +
+        today.getFullYear() +
+        "-" +
+        (today.getMonth() + 1) +
+        "-" +
+        today.getDate() +
+        " Time: " +
+        today.getHours() +
+        ":" +
+        today.getMinutes() +
+        ":" +
+        today.getSeconds();
+      newNote.dateTime = dateTimeSubmit;
+      return [newNote, ...oldAllNotes]; //อันใหม่พอกด submit จะแทรกด้านบน
+      // [...oldAllNotes, note]; อันใหม่พอกด submit จะต่อท้ายไปเรื่อยๆ
+    });
+    setNote(resetTextField);
+  }
+
+  //func delete
+  function deleteNote(note_ID) {
+    setAllNotes((oldNote) => {
+      return oldNote.filter((oneNote) => {
+        return oneNote.id !== note_ID;
+      });
+    });
+  }
+
+  //func Edit
+  const [editNote, setEditNote] = useState(null);
+  let editNoteElement = null;
+  if (!!editNote) {
+    // editNote != nil
+    editNoteElement = (
+      <div className="app-edit-note">
+        <form onSubmit={EditNoteSubmit}>
+          <p>
+            <textarea
+              rows="3"
+              placeholder="Name Content"
+              name="nameContent"
+              value={editNote.nameContent}
+              onChange={editNoteValueChange}
+            ></textarea>
+          </p>
+          <p>
+            <input
+              type="text"
+              placeholder="Name Author"
+              name="nameAuthor"
+              value={editNote.nameAuthor}
+              onChange={editNoteValueChange}
+            ></input>
+          </p>
+          <p>
+            <button type="submit">Click to Add</button>
+          </p>
+        </form>
+      </div>
+    );
+  }
+  function editNoteValueChange(event) {
+    const { name, value } = event.target;
+    setEditNote((noteBeforeEdit) => {
+      return {
+        ...noteBeforeEdit,
+        [name]: value,
+      };
+    });
+  }
+
+  function EditNoteSubmit(event) {
+    event.preventDefault();
+    setAllNotes((noteBeforeEdit) => {
+      return noteBeforeEdit.map((oneEditNote) => {
+        if (oneEditNote.id !== editNote.id) {
+          return oneEditNote;
+        } else {
+          const noteAfterEdit = { ...editNote };
+          let today = new Date(); //ลองเพิ่มการแสดงวันที่เข้าไป
+          let dateTimeSubmit =
+            "Date " +
+            today.getFullYear() +
+            "-" +
+            (today.getMonth() + 1) +
+            "-" +
+            today.getDate() +
+            " Time: " +
+            today.getHours() +
+            ":" +
+            today.getMinutes() +
+            ":" +
+            today.getSeconds();
+          editNote.dateTime = dateTimeSubmit + "(Edit)";
+          return editNote;
+        }
+      });
+    });
+    setEditNote(null);
+  }
+
+  //Elements UI ListNote
+  const noteElements = allNotes.map((oneNote) => {
+    return (
+      <div key={oneNote.id} className="app-note">
+        <h4>Author: {oneNote.nameAuthor}</h4>
+        <p>Content: {oneNote.nameContent}</p>
+        <p>{oneNote.dateTime}</p>
+        <p>
+          <a
+            onClick={() => {
+              setEditNote(oneNote);
+            }}
+          >
+            Edit
+          </a>
+          <span> | </span>
+          <a
+            onClick={() => {
+              deleteNote(oneNote.id);
+            }}
+          >
+            Delete
+          </a>
+        </p>
+      </div>
+    );
+  });
+
+  return (
+    <section className="app-section">
+      <div className="app-container">
+        <h3>Hello React</h3>
+        <form onSubmit={allNotesSubmit}>
+          <p>
+            <textarea
+              rows="3"
+              placeholder="Name Content"
+              name="nameContent"
+              value={note.nameContent}
+              onChange={noteValueChange}
+            ></textarea>
+          </p>
+          <p>
+            <input
+              type="text"
+              placeholder="Name Author"
+              name="nameAuthor"
+              value={note.nameAuthor}
+              onChange={noteValueChange}
+            ></input>
+          </p>
+          <p>
+            <button type="submit">Click to Add</button>
+          </p>
+        </form>
+        <div className="app-notes">{noteElements}</div>
+      </div>
+      {editNoteElement}
+    </section>
+  );
+}
+export default KeyBoardManager;
