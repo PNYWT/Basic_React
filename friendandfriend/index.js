e9812401babde91e506d8fe45f716e0e8903f078
PNYWT <punyawat.suwannatat@hotmail.com>
2023-03-19 22:28:36 +0700

    Handlebars (ต่อ)
    Handlebars (ต่อ)

--- friendandfriend/index.js
+++ friendandfriend/index.js
@@ -1,31 +1,38 @@
 const express = require("express");
+const hbs = require("hbs");
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
 app.set("view engine", "hbs");
+hbs.registerPartials(__dirname+"/views/partials");
 
-// Home
+const allPosts = [
+    {id: 2, titlePost: "How to investment 101 ?", userCreate:"Oni", createdDate:"18 March 2023", commentsCount:999},
+    {id: 1, titlePost: "How to be iOS Dev", userCreate:"Apple", createdDate:"19 March 2023", commentsCount:0}
+]
+
+//Routes Home
 app.get("/", (request, response) => {
-    console.log(request.query);
-    const {q, sortBy} = request.query;
-    response.render("home",{q, sortBy});
+    response.render("home",{allPosts});
 });
 
-// PostNew
+//Routes PostNew
 app.get("/p/new", (request, response) =>{
     response.render("postNew");
 });
 
-// Create New Post by
+//Routes Create New Post
 app.post("/p/new", (request, response) =>{
     const {title} = request.body ?? {};
     response.send(`Submit create New Post -> title post is ${title}`);
 });
 
-// หน้าโพสที่กดดู
+// Routes หน้าโพสที่กดดู
 app.get("/p/:post_id", (request, response) =>{
     const {post_id} = request.params;
-    response.render("postId", {post_id});
+    const onePost = allPosts.find(post => post.id === +post_id);
+    const customTitleId = !!onePost?.titlePost ? `${onePost.titlePost} | ` : `Not Found | `;
+    response.render("postId", {onePost, customTitleId});
 });
 
 app.listen(8888, () =>{
