f14fdea18044fda6c1292a87e5e37d11dbcc25e3
PNYWT <punyawat.suwannatat@hotmail.com>
2023-03-19 19:47:11 +0700

    update
    Express + Handlebars Template

--- friendandfriend/index.js
+++ friendandfriend/index.js
@@ -2,29 +2,30 @@ const express = require("express");
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
+app.set("view engine", "hbs");
 
-//callback
+// Home
 app.get("/", (request, response) => {
-    console.log(request.query)
+    console.log(request.query);
     const {q, sortBy} = request.query;
-    response.send(`Hello request.query -> q=${q}, photo_id=${sortBy}`)
+    response.render("home",{q, sortBy});
 });
 
+// PostNew
 app.get("/p/new", (request, response) =>{
-    response.send("Read New Post")
+    response.render("postNew");
 });
 
+// Create New Post by
 app.post("/p/new", (request, response) =>{
-    console.log(request.body)
     const {title} = request.body ?? {};
-    response.send(`Submit create New Post -> title post is ${title}`)
+    response.send(`Submit create New Post -> title post is ${title}`);
 });
 
-///p/:ชื่อตัวแปร id ที่ต้องการรับเข้ามาค้นหา เช่น โพสที่เท่าไหร่ก็อาจจะตั้งเป็น post_id รูปที่เท่าไหร่ร่ก็อาจจะตั้งเป็น photo_id อะไรประมาณนั้น
+// หน้าโพสที่กดดู
 app.get("/p/:post_id", (request, response) =>{
-    console.log(request.params)
-    const {post_id} = request.params
-    response.send(`Hello params -> post_id is ${post_id}`)
+    const {post_id} = request.params;
+    response.render("postId", {post_id});
 });
 
 app.listen(8888, () =>{
