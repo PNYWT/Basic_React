f38b2a0b3a3f9cdbde7a69f5f80127638101e21f
PNYWT <punyawat.suwannatat@hotmail.com>
2023-04-06 16:44:18 +0700

    update
    Static file
    Css normalize and font
    set css in HomePage

--- friendandfriend/index.js
+++ friendandfriend/index.js
@@ -4,9 +4,12 @@ const app = express();
 const generalRouters = require("./routers/generalRouters");
 const postsRouter = require("./routers/postsRouter");
 
+//ส่งข้อมูลมาทาง body HTML
 app.use(express.urlencoded({extended: true}));
 app.set("view engine", "hbs");
 hbs.registerPartials(__dirname+"/views/partials");
+//แชร์ static file ที่ต้องการใช้
+app.use("/staticFolder", express.static("staticFolder"));
 
 //call generalRouters Home
 app.use("/", generalRouters);
